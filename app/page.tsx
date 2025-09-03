7b5cfa04b4c6398648a9f57982e2044c9cb3e804
MetaTechGuys <Sepideh.Golestani.MTG@gmail.com>
2025-09-03 18:46:08 +0330

    v0.1.2

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,43 @@ export default function Home() {
         priority
       />
       <Procover
-        src="/images/MTG.sitebanner.v1.01.00.1404.06.05-04.webp"
+        src="/images/MTG-01.webp"
         alt="Richmind cover"
         title="RichMind Holding"
         subtitle="Future of Digital Marketing"
+        priority
+        button={{
+          text: "Explore More",
+          href: "/projects"
+        }}
+      />
+      <Procover
+        src="/images/MTG-02.webp"
+        alt="RVA cover"
+        title="Royal Virtual Assets"
+        subtitle="Future of Digital Assets"
+        priority
+        button={{
+          text: "Explore More",
+          href: "/projects"
+        }}
+      />
+      <Procover
+        src="/images/MTG-04.webp"
+        alt="Capital Address Group cover"
+        title="Capital Address Group"
+        subtitle="Future of Economy"
+        priority
+        button={{
+          text: "Explore More",
+          href: "/projects"
+        }}
+      />
+      <Procover
+        src="/images/MTG-05.webp"
+        alt="Asar Sazan cover"
+        title="Asar Sazan Properties"
+        subtitle="Future of Architecture"
         priority
         button={{
           text: "Explore More",
